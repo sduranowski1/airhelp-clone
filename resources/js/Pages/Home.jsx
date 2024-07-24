5c8791d18c728dfb33468133d69dfc515f8d4720
sduranowski1 <sduranowski1@gmail.com>
2024-07-24 16:50:03 +0200

    init #20

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -113,7 +113,7 @@ const HomeContent = () => {
             <div>
                 {/*<div className="home-hero sm:px-6 lg:px-8 container-custom  relative">*/}
                     <div className="home-hero sm:px-6 lg:px-8 container-custom relative" style={{
-                        backgroundImage: 'url("media/home_hero.png")',
+                        backgroundImage: 'url("media/hero_improved.png")',
                         backgroundRepeat: 'no-repeat',
                         backgroundPosition: 'bottom right',
                         backgroundSize: '40%',
