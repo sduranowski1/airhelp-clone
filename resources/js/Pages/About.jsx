c1641d8b83c60542bf8ba0a070cbb36287d7051d
Simon Duranowski <sduranowski1@gmail.com>
2024-11-21 14:24:55 +0100

    feat: about us images

--- resources/js/Pages/About.jsx
+++ resources/js/Pages/About.jsx
@@ -19,7 +19,7 @@ const AboutUs = () => {
 
                         <section>
                             <h2>Kim jesteśmy?</h2>
-                            <img src="https://media.geeksforgeeks.org/wp-content/uploads/20230805110219/a.png" className="w-full h-96 object-cover mb-4 rounded"/>
+                            <img src="http://138.68.158.146/media/about/Canceled-Flights.jpg" className="w-full h-96 object-cover mb-4 rounded"/>
                             <p>BeSmartAir to wiodąca firma specjalizująca się w pomocy pasażerom w dochodzeniu odszkodowań za opóźnione, odwołane lub przesiadkowe loty. Nasza misja to zapewnienie, że każdy pasażer ma prawo do rekompensaty, której jest wart, niezależnie od sytuacji.</p>
 
                             <h3>Nasza Ekspertyza</h3>
@@ -27,8 +27,7 @@ const AboutUs = () => {
                                 przepisów prawa europejskiego, w tym Rozporządzenia WE 261/2004, które chroni prawa pasażerów. Dzięki temu jesteśmy w stanie skutecznie reprezentować naszych klientów w negocjacjach z liniami lotniczymi.</p>
 
                             <h3>Nasza Usługa</h3>
-                            <img src="https://media.geeksforgeeks.org/wp-content/uploads/20230805110219/a.png"
-                                 className="w-full h-96 object-cover mb-4 rounded"/>
+                            <img src="http://138.68.158.146/media/about/jeshoots-com-mSESwdMZr-A-unsplash.jpg"                                 className="w-full h-96 object-cover mb-4 rounded"/>
                             <p>W BeSmartAir oferujemy kompleksową obsługę – od analizy przypadków, przez zbieranie
                                 niezbędnych dokumentów, aż po reprezentowanie klientów w postępowaniach przed liniami lotniczymi. Nasz proces jest prosty i przejrzysty: klienci mogą zgłosić swoje roszczenie online, a my zajmiemy się resztą.</p>
 
@@ -37,7 +36,7 @@ const AboutUs = () => {
                                 że ich prawa są w dobrych rękach. Nasza firma działa na zasadzie "no win, no fee", co oznacza, że płacisz tylko w przypadku sukcesu.</p>
 
                             <h3>Dołącz do nas!</h3>
-                            <img src="https://media.geeksforgeeks.org/wp-content/uploads/20230805110219/a.png"
+                            <img src="http://138.68.158.146/media/about/warsaw-transport-airport-AB-XL.jpg"
                                  className="w-full h-96 object-cover mb-4 rounded"/>
 
                             <p>Dołącz do grona zadowolonych klientów BeSmartAir i odzyskaj swoje pieniądze za
